7b300ffe5089f3fde5b6cee2c0767049ff15a95c
Dinusha Madhuranga <dmadhuranga@mitrai.com>
2021-07-19 01:01:06 +0530

    working hour bug fix

--- src/app/shop/shop-registration/shop-registration.component.ts
+++ src/app/shop/shop-registration/shop-registration.component.ts
@@ -873,7 +873,7 @@ export class ShopRegistrationComponent implements OnInit {
     if (time != null) {
       let hour = (time.split(':'))[0]
       let min = (time.split(':'))[1]
-      let part = hour > 12 ? 'PM' : 'AM';
+      let part = hour >= 12 ? 'PM' : 'AM';
       min = (min + '').length == 1 ? `0${min}` : min;
       hour = hour > 12 ? hour - 12 : hour;
       hour = (hour + '').length == 1 ? `0${hour}` : hour;
