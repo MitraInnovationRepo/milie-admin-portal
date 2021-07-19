cad0fbd77c671317015dfbfed85ec3e2fda7d2a2
Dinusha Madhuranga <dmadhuranga@mitrai.com>
2021-07-19 16:04:43 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   AUTH_URL:'auth',
   API_URL:'api',
-  CLIENT_ID: 'foodie-mobile',
-  REALM: 'foodie', 
-  CLIENT_SECRET: '5c5450dc-6f10-4e47-a52a-3c9dcc4e776f'
+  CLIENT_ID: 'milie-mobile',
+  REALM: 'milie', 
+  CLIENT_SECRET: '06ce2154-ed4b-464e-89fe-07f1aa17d409'
 };
